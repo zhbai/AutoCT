fd1c7b8279270f3fa3c854c5acbcd1b4475a0ed3
zhbai <zhebai@lbl.gov>
2023-08-18 09:00:10 -0700

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='autoct_registration',
-    version='0.1.0',
+    version='1.1',
     include_package_data=True,
     url='https://bitbucket.org/LBL_TBI/tbi_registration/',
     author='Zhe Bai, Abdelilah Essiari',
